40e88fe5c10a2953abfa64ea0dc0f334ebb03449
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-02-21 12:03:56 +0530

    sample get and create blogs api

--- backend/controllers/blogsController.js
+++ backend/controllers/blogsController.js
@@ -11,6 +11,36 @@ const getBlogs = async (req, res) => {
     }
 }
 
+const createBlog = async (req, res) => {
+    try {
+        const { title, description, slug, content, author, category, tags, coverImage, publishedAt } = req.body;
+
+        // Validate required fields
+        if (!title || !slug || !content || !author || !category) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        const newBlog = new Blog({
+            title,
+            description,
+            slug,
+            content,
+            author,
+            category,
+            tags,
+            coverImage,
+            publishedAt
+        });
+
+        await newBlog.save();
+        res.status(201).json({ message: "Blog created successfully", blog: newBlog });
+    } catch (error) {
+        console.error("Error during blog creation:", error);
+        res.status(500).json({ error: error.message });
+    }
+};
+
 export {
     getBlogs,
+    createBlog,
 }
--- backend/routers/blogsRouter.js
+++ backend/routers/blogsRouter.js
@@ -2,6 +2,15 @@ import express from 'express';
 const router = express.Router();
 import verifyAdminToken from '../middlewares/adminAuthMiddleware.js';
 import upload from '../middlewares/multer.js';
-// import
+import {
+    getBlogs,
+    createBlog,
+} from '../controllers/blogsController.js';
+
+// This middleware will be applied to all the route below this middleware
+router.use(verifyAdminToken);
+
+router.get('/', getBlogs);
+router.post('/', createBlog);
 
 export default router;
