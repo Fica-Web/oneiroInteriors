f01a90cf8f034d9e18898186b02796832191ab05
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-02-25 11:06:39 +0530

    added new font in css

--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div>
-            HOME PAGE 
+        <div className='ForosMedium foros-medium font-semibold text-5xl'>
+            HOME PAGE Transform
         </div>
     )
 }
