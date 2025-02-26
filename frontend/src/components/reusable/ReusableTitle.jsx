56ec3c3bcef957195bcaab343bb5c98a06e3bb0e
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-02-26 15:42:52 +0530

    sample reusable title card

---
+++ frontend/src/components/reusable/ReusableTitle.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ReusableTitle = ({ page, title, description }) => {
+    return (
+        <div className='w-full border-t border-gray-600'>
+            <div className='w-11/12 mx-auto my-12'>
+                <div className='flex items-center gap-2'>
+                    <div className='h-0.5 w-12 bg-primary'></div>
+                    <h3>{ page }</h3>
+                </div>
+                <h2 className='text-5xl ackeler-a'>
+                    { title }
+                </h2>
+                <p className='mt-5'>
+                    { description }
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ReusableTitle
--- frontend/src/pages/user/AboutPage.jsx
+++ frontend/src/pages/user/AboutPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import ReusableTitle from '../../components/reusable/ReusableTitle';
 
 const AboutPage = () => {
     return (
         <div>
-            About page
+            <ReusableTitle 
+                page='About Us'
+                title='Who we are'
+                description='Welcome to Oneiro, where every space tells a story of innovation, passion, and unparalleled craftsmanship. Established in 2018, Koortsi has evolved into a distinguished name in the realms of architecture and interior design.'
+            />
         </div>
     )
 }
