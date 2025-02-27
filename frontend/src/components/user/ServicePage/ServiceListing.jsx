de338877d586d89650e558160b61c072e956abc7
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-02-27 11:23:06 +0530

    updated ServiceListing component

--- frontend/src/components/user/ServicePage/ServiceListing.jsx
+++ frontend/src/components/user/ServicePage/ServiceListing.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const services = [
     { id: 2, title: "Commercial Spaces", description: "Enhance productivity and brand presence with our innovative commercial space designs.", image: "https://media.designcafe.com/wp-content/uploads/2022/07/29185246/tv-unit-design-in-the-living-room-features-floating-cabinet.jpg" },
     { id: 3, title: "Space Planning", description: "Optimize your space with our expert planning solutions for a functional and aesthetic layout.", image: "https://www.venzahomedecorss.com/blog/wp-content/uploads/2020/10/venza-blog.jpg" },
     { id: 4, title: "Furniture & Decor", description: "Curate the perfect ambiance with our handpicked furniture and decor elements.", image: "https://media.designcafe.com/wp-content/uploads/2024/12/27181027/interior-design-trends-in-2025.jpg" },
-    { id: 1, title: "Commercial Spaces", description: "Enhance productivity and brand presence with our innovative commercial space designs.", image: "https://media.designcafe.com/wp-content/uploads/2022/07/29185246/tv-unit-design-in-the-living-room-features-floating-cabinet.jpg" },
-    { id: 5, title: "Space Planning", description: "Optimize your space with our expert planning solutions for a functional and aesthetic layout.", image: "https://www.venzahomedecorss.com/blog/wp-content/uploads/2020/10/venza-blog.jpg" },
-    { id: 6, title: "Furniture & Decor", description: "Curate the perfect ambiance with our handpicked furniture and decor elements.", image: "https://media.designcafe.com/wp-content/uploads/2024/12/27181027/interior-design-trends-in-2025.jpg" },
+    { id: 1, title: "Residential Interiors", description: "Transform your home into a personalized haven with our interior solutions.", image: "https://media.designcafe.com/wp-content/uploads/2022/07/29185246/tv-unit-design-in-the-living-room-features-floating-cabinet.jpg" },
+    { id: 5, title: "Office Interiors", description: "Design efficient and inspiring office spaces tailored to your business needs.", image: "https://www.venzahomedecorss.com/blog/wp-content/uploads/2020/10/venza-blog.jpg" },
+    { id: 6, title: "Luxury Interiors", description: "Experience sophistication with our exclusive luxury interior designs.", image: "https://media.designcafe.com/wp-content/uploads/2024/12/27181027/interior-design-trends-in-2025.jpg" },
 ];
 
-const ServiceListing = () => {
+const ServiceListing = ({ isHomePage }) => {
     const [selectedService, setSelectedService] = useState(null);
 
+    // Show only first 3 services if it's the homepage
+    const displayedServices = isHomePage ? services.slice(0, 3) : services;
+
     return (
-        <div className="w-11/12 mx-auto my-10">
-            <h2 className="text-5xl ackeler-a font-bold text-center mb-8">Our Services</h2>
+        <div className="w-11/12 mx-auto my-10 lg:my-20">
+            <h2 className="text-5xl text-center mb-8 ackeler-a">Our Services</h2>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {services.map((service) => (
-                    <div 
-                        key={service.id} 
-                        className={`relative overflow-hidden  rounded-lg shadow-md cursor-pointer transition-all duration-300 transform hover:scale-105 ${selectedService === service.id ? 'bg-gray-200' : 'bg-white hover:bg-gray-100'}`}
+                {displayedServices.map((service) => (
+                    <div
+                        key={service.id}
+                        className={`relative overflow-hidden rounded-lg shadow-md cursor-pointer transition-all duration-300 transform hover:scale-105 ${selectedService === service.id ? 'bg-gray-200' : 'bg-white hover:bg-gray-100'}`}
                         onClick={() => setSelectedService(service.id)}
                     >
                         <div className="relative group">
@@ -32,6 +36,16 @@ const ServiceListing = () => {
                     </div>
                 ))}
             </div>
+
+            {
+                isHomePage && (
+                    <div className='flex justify-center my-10'>
+                        <Link to={'/services'} className='bg-gray-500 p-2 px-8 rounded-md text-white font-semibold hover:scale-105 transition-transform duration-300'>
+                            Explore More
+                        </Link>
+                    </div>
+                )
+            }
         </div>
     );
 };
--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeCarousel from '../../components/user/HomePage/HomeCarousal';
 import Hero from '../../components/user/HomePage/Hero';
+import ServiceListing from '../../components/user/ServicePage/ServiceListing';
 import HowWeWork from '../../components/user/HomePage/HowWeWork';
 import BrandsWeWorkWith from '../../components/user/HomePage/BrandsWeWorkWith';
 
@@ -9,6 +10,7 @@ const HomePage = () => {
         <div>
             <HomeCarousel />
             <Hero />
+            <ServiceListing isHomePage={true} />
             <HowWeWork />
             <BrandsWeWorkWith />
         </div>
