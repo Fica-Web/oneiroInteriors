a972ba10f857d272b0459e4fa183a26e380c9403
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-04 11:57:09 +0530

    whatsappButton added

---
+++ frontend/src/components/shared/WhatsappButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+
+const WhatsAppButton = () => {
+  const phoneNumber = "919633567287"; // Replace with your WhatsApp number
+  const message = "Hi! I would like to know more about Oneiro Interiors.";
+
+  return (
+    <a
+      href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-5 right-5 z-40 bg-green-500 text-white rounded-full w-16 h-16 lg:w-20 lg:h-20 flex items-center justify-center shadow-lg hover:bg-green-600 transition-all sm:bottom-4 sm:right-4 sm:w-14 sm:h-14"
+    >
+      <FaWhatsapp className="text-2xl sm:text-xl lg:text-5xl" />
+    </a>
+  );
+};
+
+export default WhatsAppButton;
--- frontend/src/layoutes/HomeLayout.jsx
+++ frontend/src/layoutes/HomeLayout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/shared/Navbar';
 import Footer from '../components/shared/Footer';
+import WhatsAppButton from '../components/shared/WhatsappButton';
 
 const HomeLayout = () => {
     return (
         <>
             <Navbar />
+            <WhatsAppButton />
             <Outlet />
             <Footer />
         </>
