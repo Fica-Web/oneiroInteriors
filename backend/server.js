652bedd388297671b2a576b57145a7f92eb0e4b8
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-03-12 16:49:37 +0530

    added https in crosOptions

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 
 // CORS options to allow requests from specific origin
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://oneirointeriors.in',], // Allow requests from this frontend domain
+    origin: ['http://localhost:5173', 'http://oneirointeriors.in', 'https://oneirointeriors.in',], // Allow requests from this frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Include cookies in requests
 };
@@ -33,11 +33,6 @@ app.use(morgan('tiny')); // Log HTTP requests using morgan's 'tiny' format
 app.use(cors(corsOptions)); // Enable CORS using the specified options
 
 // Define route handlers
-// app.use('/user', userAuthRouter); // Routes for user authentication
-// app.use('/post', postRouter); // Routes for posts (create, delete, etc.)
-// app.use('/follow', followRouter); // Routes for following/unfollowing users
-// app.use('/messages', messageRouter); // Routes for message-specific functionalities
-// app.use('/notification', notificationRouter); // Routes for notification-specific functionalities
 app.use('/api/admin', adminRouter); // Routes for admin-specific functionalities
 app.use('/api/blogs', blogsRouter); // Routes for blogs-specific functionalities
 app.use('/api/carousel', carousalRouter); // Routes for carousal-specific functionalities
