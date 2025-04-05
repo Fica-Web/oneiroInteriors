bc269e1735dcea85248272e474ecfa7a744ab26d
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-04-05 14:19:10 +0530

    added url in corsOrigin

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 
 // CORS options to allow requests from specific origin
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://oneirointeriors.in', 'https://oneirointeriors.in',], // Allow requests from this frontend domain
+    origin: ['http://localhost:5173', 'http://oneirointeriors.in', 'https://oneirointeriors.in', 'https://www.oneirointeriors.in',], // Allow requests from this frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Include cookies in requests
 };
